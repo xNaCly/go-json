3e753079b3804cdbff589373995af7d1941d3946
samuelscheit <github@samuelscheit.com>
2025-01-17 20:24:25 +0100

    chore: remove go and js recursion test + merge in gen.py

--- test/bench.sh
+++ test/bench.sh
@@ -9,3 +9,9 @@ go build ./test.go
 hyperfine "./test ./1MB.json" "./test -libjson=false ./1MB.json"
 hyperfine "./test ./5MB.json" "./test -libjson=false ./5MB.json"
 hyperfine "./test ./10MB.json" "./test -libjson=false ./10MB.json"
+
+hyperfine "./test ./1K_recursion.json" "./test -libjson=false ./10MB.json"
+hyperfine "./test ./10_recursion.json" "./test -libjson=false ./10MB.json"
+hyperfine "./test ./100K_recursion.json" "./test -libjson=false ./10MB.json"
+hyperfine "./test ./1M_recursion.json" "./test -libjson=false ./10MB.json"
+hyperfine "./test ./10M_recursion.json" "./test -libjson=false ./10MB.json"
